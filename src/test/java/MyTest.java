40407c477c1e700a0517a7413d4fb19a2dc66209
tom_glb <goodMorning_glb@atguigu.com>
2019-04-16 13:28:02 +0800

    first

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author zhangliang
+ *
+ */
+public class MyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
